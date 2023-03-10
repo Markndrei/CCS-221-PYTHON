53d1a49d7ed59964c813b97a59a14503d8d60c49
Markndrei <119771465+Markndrei@users.noreply.github.com>
2023-03-11 01:52:32 +0800

    Update applicatiorisation.py

--- applicatiorisation.py
+++ applicatiorisation.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import numpy as np
 import matplotlib as plt
+import opencv as cv2
 
 def main ():
     st.title ("Activity 3: Image Manipulator")
+    Manipulation = ["Translation", "Rotation", "Scaling", "Shear", "Reflection"]
+    choice = st.sidebar.selectbox("Manipulation", Manipulation)
+    
     
 if __name__ == '__main__' :
     main()
