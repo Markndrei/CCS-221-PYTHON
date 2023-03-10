9a99c51aa2edd6401a6152d343fc6b8281fc82c3
Markndrei <119771465+Markndrei@users.noreply.github.com>
2023-03-11 01:54:57 +0800

    Update applicatiorisation.py

--- applicatiorisation.py
+++ applicatiorisation.py
@@ -8,6 +8,19 @@ def main ():
     Manipulation = ["Translation", "Rotation", "Scaling", "Shear", "Reflection"]
     choice = st.sidebar.selectbox("Manipulation", Manipulation)
     
+    if choice == "Translation" :
+        m_translation_ = np.float32([[1, 0, 100],
+                                    [0, 1, 50],
+                                    [0, 0, 1]
+                                 ])
+    
+        fig, translated_image = cv2.warpPerspective(image, m_translation_, (cols, rows))
+    
+        plt.axis('off')
+        plt.imshow(translated_image)
+        plt.show()
+        st.pyplot (fig)
+    
     
 if __name__ == '__main__' :
     main()
