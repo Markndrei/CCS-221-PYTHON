0992fb2957794131ce0ca594917cc02f21f0410e
Markndrei <119771465+Markndrei@users.noreply.github.com>
2023-03-11 01:36:06 +0800

    Update applicatiorisation.py

--- applicatiorisation.py
+++ applicatiorisation.py
@@ -4,7 +4,7 @@ import cv2
 import matplotlib.pyplot as plt
 import streamlit as st
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+uploaded_files = st.file_uploader("Choose a PNG file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
