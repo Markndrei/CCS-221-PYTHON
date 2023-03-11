c562469c6306cd49fa69b812ff60c562a1c0824f
Markndrei <119771465+Markndrei@users.noreply.github.com>
2023-03-11 16:53:26 +0800

    Update applicatiorisation.py

--- applicatiorisation.py
+++ applicatiorisation.py
@@ -1,196 +1,124 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import streamlit as st
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from scipy.spatial import Delaunay
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+st.title("Activity3\nGroup6\nImages")
+# Reading of Image
+png = str(".png")
+fig = plt.figure()
+address = str("pages/")
 
-import tensorflow as tf
-
-tf.compat.v1.disable_eager_execution()
-
-option=[]    
-
-def _cube_(bottom_lower=(0, 0, 0), side_length=3):
-    """Create cube starting from the given bottom-lower point (lowest x, y, z values)"""
-    bottom_lower = np.array(bottom_lower)
+def translation(i):
     
-    points = np.vstack([
-        bottom_lower,
-        bottom_lower + [0, side_length, 0],
-        bottom_lower + [side_length, side_length, 0],
-        bottom_lower + [side_length, 0, 0],
-        bottom_lower + [0, 0, side_length],
-        bottom_lower + [0, side_length, side_length],
-        bottom_lower + [side_length, side_length, side_length],
-        bottom_lower + [side_length, 0, side_length],
-        bottom_lower,
-        
-    ])   
-
-
-    return points
-def _pyramid_(side_length=1):
-
-    points = np.vstack([
-            ([[-1, -1, -1],
-                [side_length, -1, -1 ],
-                [side_length, side_length, -1],
-                [-1, side_length, -1],
-                [0, 0 , side_length]])
-    ])        
-         
-    return points
-
-
-
-def _rectangle_(bottom_lower=(0, 0, 0), side_length=2):
-    """Create cube starting from the given bottom-lower point (lowest x, y, z values)"""
-    bottom_lower = np.array(bottom_lower)
+    #Translation
+    m_translation_ = np.float32([[1, 0, 100],
+                                 [0, 1, 200],
+                                 [0, 0, 1]])
     
-    points = np.vstack([
-        bottom_lower,
-        [side_length, 0, 0],
-        [side_length, 4, 0],
-        [0, 4, 0],
-        [0, 0, 3],
-        [side_length, 0, 3],
-        [side_length, 4, 3],
-        [0, 4, 3]
-    ])   
-    
-    return points
+ 
+    img_ = cv2.imread(address + str(i) + png)
+    img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2RGB)
+    cols, rows = (img_.shape[:2])
 
-
-
-def _diamond_(side_length=1):
-
-    points = np.vstack([
-                ([[-1, -1, -1],
-                [side_length, -1, -1 ],
-                [side_length, side_length, -1],
-                [-1, side_length, -1],
-                [0, 0 , side_length],
-                [0, 0, -3]
-                ])
-    ])        
-    
-    return points   
-
-def _plt_basic_object(points):
-    """Plots a basic object, assuming its convex and not too complex"""
-    
-    tri = Delaunay(points).convex_hull
-    
-    fig = plt.figure(figsize=(8,8))
-    ax = fig.add_subplot(111, projection='3d')
-    S = ax.plot_trisurf(points[:,0], points[:,1],
-                        points[:,2], triangles=tri,
-                        shade=True, cmap=cm.rainbow,
-                        lw=0.5
-                        )
-    
-    ax.set_xlim3d(-5, 5)
-    ax.set_ylim3d(-5, 5)
-    ax.set_zlim3d(-5, 5)
-    
-
+    translated_img_ = cv2.warpPerspective(img_, m_translation_, (cols, rows))
+    plt.axis('off')
+    plt.imshow(translated_img_)
     plt.show()
-    st.pyplot (fig)
+    st.pyplot(fig)
 
+def rotation(i):
+    
+    # Rotation
+    angle = np.radians(10)
+    m_rotation_ = np.float32([[np.cos(angle), -(np.sin(angle)), 0],
+                              [np.sin(angle), np.cos(angle), 0],
+                              [0, 0, 1]])
+    
+   
+    img_ = cv2.imread(address + str(i) + png)
+    img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2RGB)
+    cols, rows = img_.shape[:2] 
 
+    rotated_img_ = cv2.warpPerspective(img_, m_rotation_, (int(cols), int(rows)))
+    plt.axis('off')
+    plt.imshow(rotated_img_)
+    plt.show()
+    st.pyplot(fig)
+    
+def scaling(i):
+    
+    # Scaling
+    m_scaling_ = np.float32([[1.5, 0, 0],
+                             [0, 1.8, 0],
+                             [0, 0, 1]])
+    
+   
+    img_ = cv2.imread(address + str(i) + png)
+    img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2RGB)
+    cols, rows = img_.shape[:2] 
 
+    scaled_img_ = cv2.warpPerspective(img_, m_scaling_, (cols*2, rows*2))
+    plt.axis('off')
+    plt.imshow(scaled_img_)
+    plt.show()
+    st.pyplot(fig)
+    
+def shear(i):
+    
+    # Shearing
+    m_shearing_x = np.float32([[1, 0.5, 0],
+                               [0, 1, 0],
+                               [0, 0, 1]])
+    
+    
+    img_ = cv2.imread(address + str(i) + png)
+    img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2RGB)
+    cols, rows = img_.shape[:2]
 
-init_cube_ = _cube_(side_length=3)
-init_pyramid_ = _pyramid_(side_length=1)
-init_rectangle_ = _rectangle_(side_length=3)
-init_diamond_ = _diamond_(side_length=1)
+    sheared_img_x = cv2.warpPerspective(img_,m_shearing_x,(int(cols*1.5), int(rows*1.5)))
+    plt.axis('off')
+    plt.imshow(sheared_img_x)
+    plt.show()
+    st.pyplot(fig)
 
-def translate(points):
-    def translate_obj(points, amount):
-        return tf.add(points, amount)
+def reflection(i):
+    
+    # Reflection
+    
+    img_ = cv2.imread(address + str(i) + png)
+    img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2RGB)
+    cols, rows = img_.shape[:2]
+    
+    m_reflection_ = np.float32([[1, 0, 0],
+                                [0, -1, rows],
+                                [0, 0, 1]])
+    
+  
 
+    reflected_img_ = cv2.warpPerspective(img_, m_reflection_,(int(cols), int(rows)))
+    plt.axis('off')
+    plt.imshow(reflected_img_)
+    plt.show()
+    st.pyplot(fig)
 
-    translation_amount = tf.constant([1, 2, 2], dtype=tf.float32)
-    translated_shape = translate_obj(points, translation_amount)
-
-
-    with tf.compat.v1.Session() as session:
-
-        translated_shape = session.run(translated_shape) 
-
-    _plt_basic_object(translated_shape)
-
-def rotate(option, points):
-    def rotate_obj(points, angle):
-        angle = float(angle)
-        rotation_matrix = tf.stack([
-                        [tf.cos(angle), tf.sin(angle), 0],
-                        [-tf.sin(angle), tf.cos(angle), 0],
-                        [0, 0, 1]
-        ])
-
-        rotate_object = tf.matmul(tf.cast(points, tf.float32), tf.cast(rotation_matrix, tf.float32))
-        
-        return rotate_object
-        
-        
-    with tf.compat.v1.Session() as session:
-            
-        if option == "Cube":
-            rotated_object = session.run(rotate_obj(init_cube_, 75)) 
-            _plt_basic_object(rotated_object)
-            
-        if option == "Pyramid":
-            rotated_object = session.run(rotate_obj(init_pyramid_, 75)) 
-            _plt_basic_object(rotated_object)
-            
-        if option == "Rectangle":
-            rotated_object = session.run(rotate_obj(init_rectangle_, 75)) 
-            _plt_basic_object(rotated_object)
-            
-        if option == "Diamond":
-            rotated_object = session.run(rotate_obj(init_diamond_, 75)) 
-            _plt_basic_object(rotated_object)        
-            
-def main():
-    option = st.selectbox('What shape would you like to rotate?', ('Cube', 'Pyramid', 'Rectangle', 'Diamond'))
-
-    st.write('The shape you chose is:', option)
-
-    if option == "Cube":
-        _cube_(bottom_lower=(0, 0, 0), side_length=3)
-        init_cube_ = _cube_(side_length=3)
-        points = tf.constant(init_cube_, dtype=tf.float32)
-        translate(points)
-        rotate(option, points)
-        
-    if option == "Pyramid":
-        _pyramid_(side_length=1)
-        init_pyramid_ = _pyramid_(side_length=1)
-        points = tf.constant(init_pyramid_, dtype=tf.float32)
-        translate(points)
-        rotate(option, points)
-            
-    if option == "Rectangle":
-        _rectangle_(side_length=3)
-        init_pyramid_ = _rectangle_(side_length=3)
-        points = tf.constant(init_rectangle_, dtype=tf.float32)
-        translate(points)
-        rotate(option, points)
-            
-    if option == "Diamond":
-        _diamond_(side_length=1)
-        init_pyramid_ = _diamond_(side_length=1)
-        points = tf.constant(init_diamond_, dtype=tf.float32)
-        translate(points)
-        rotate(option, points)  
-        
+def main () :
+    i = st.slider('Choose Image (1-3)', 1, 3, 1)
+    
+    st.write("Translation")
+    translation(i)
+    
+    st.write("Rotation")
+    rotation(i)
+    
+    st.write("Scale")
+    scaling(i)
+    
+    st.write("Shear")
+    shear(i)
+    
+    st.write("Reflection")
+    reflection(i)
+    
+    
 if __name__ == '__main__':
     main()
-
-
-
-
-       
-               
