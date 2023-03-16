7dbc2fa809bfba65b533b292527713e6ef20f669
Markndrei <119771465+Markndrei@users.noreply.github.com>
2023-03-16 21:09:45 +0800

    Update applicatiorisation.py

--- applicatiorisation.py
+++ applicatiorisation.py
@@ -16,7 +16,7 @@ def translation(images):
  
     images = Image.open(images)
     images = np.asarray(images)
-    cols, rows = (image.shape[:2])
+    cols, rows = (images.shape[:2])
     translated_image = cv2.warpPerspective(images, m_translation_, (cols, rows))
     plt.axis('off')
     plt.imshow(translated_image)
